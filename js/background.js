4dc3fc32061a3ebcd2d9390f333f866d65d46bf7
yhl452493373 <yhl452493373@gmail.com>
2022-05-16 16:36:42 +0800

    1、又字符串判断改为变量判断

--- js/background.js
+++ js/background.js
@@ -86,7 +86,7 @@ let downloading = false;
 chrome.downloads.onCreated.addListener(downloadItemInfo => {
     let existItem = downloadingItems.find(item => item.id === downloadItemInfo.id);
     let downloadItem = new DownloadItem(downloadItemInfo);
-    if (!existItem && downloadItemInfo.state !== State.complete && downloadItemInfo.state !== State.interrupted) {
+    if (!existItem && downloadItem.state !== State.complete && downloadItem.state !== State.interrupted) {
         downloadingItems.push(downloadItem);
     }
     startPolling();
